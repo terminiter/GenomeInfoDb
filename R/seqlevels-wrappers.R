cec205a19f56fc725067a2e2081cb0324317556f
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-16 20:00:01 +0000

    Creating branch for BioC 3.1 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/GenomeInfoDb@102593 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/seqlevels-wrappers.R
@@ -0,0 +1,114 @@
+### =========================================================================
+### Convenience wrappers to the seqlevels() getter and setter
+### -------------------------------------------------------------------------
+
+keepSeqlevels <- function(x, value, ...)
+{
+    value <- unname(value)
+    if (any(nomatch <- !value %in% seqlevels(x)))
+        warning("invalid seqlevels '", 
+                paste(value[nomatch], collapse=","), "' were ignored")
+    if (is(x, "BSgenome"))
+        stop("seqlevels cannot be dropped from a BSgenome object")
+    force <- is(x, "Seqinfo")
+    seqlevels(x, force=!force) <- value[!nomatch]
+    x
+}
+
+dropSeqlevels <- function(x, value, ...)
+{
+    value <- unname(value)
+    if (any(nomatch <- !value %in% seqlevels(x)))
+        warning("invalid seqlevels '", 
+                paste(value[nomatch], collapse=","), "' were ignored")
+    if (is(x, "BSgenome"))
+        stop("seqlevels cannot be dropped from a BSgenome object")
+    force <- is(x, "Seqinfo")
+    seqlevels(x, force=!force) <- seqlevels(x)[!seqlevels(x) %in% value]
+    x
+}
+
+renameSeqlevels <- function(x, value, ...)
+{
+    nms <- names(value)
+    ## unnamed
+    if (is.null(nms)) {
+        if (length(value) != length(seqlevels(x)))
+            stop("unnamed 'value' must be the same length as seqlevels(x)")
+        names(value) <- seqlevels(x)
+    ## named
+    } else {
+        if (any(nomatch <- !nms %in% seqlevels(x)))
+            warning("invalid seqlevels ", 
+                    paste(sQuote(nms[nomatch]), collapse=", "), " ignored")
+        if (length(value) != length(seqlevels(x))) {
+            level <- seqlevels(x)
+            idx <- match(nms, level)
+            idx <- idx[complete.cases(idx)]
+            value <- replace(level, idx, value[seq_along(idx)])
+        } 
+    } 
+    seqlevels(x) <- value  
+    x 
+}
+
+## Currently applies to TxDb only.
+restoreSeqlevels <- function(x, ...)
+{
+    seqlevels0(x) 
+    x
+}
+
+keepStandardChromosomes <- function(x, species=NULL)
+{
+    ori_seqlevels <- seqlevels(x)
+    
+    if(length(ori_seqlevels)==0)
+        return(x)
+    
+    ans <- .guessSpeciesStyle(ori_seqlevels)
+    
+    if(length(ans)==1){
+        if(is.na(ans)){
+            ## Interanally the seqlevels did not match any organism's style - so
+            ## drop all levels and return an empty object
+            return(dropSeqlevels(x, seqlevels(x), force=TRUE))
+        }
+    }
+    
+    style <- unique(ans$style)
+    standard_chromosomes <- character(0)
+    
+    if(missing(species))
+    {
+        ## 1- find compatible species based on style of Seqinfo object
+        ## 2- extract seqlevels for all compatible species. 
+        ## 3- intersect, find how many species have some seqlevels as original?
+        allspecies <- names(genomeStyles())
+        
+        compatibility_idx <- sapply(genomeStyles(),
+                                    function(y) style %in% colnames(y))
+        compatible_species <- names(compatibility_idx)[compatibility_idx]
+        allseqlevels <- lapply(compatible_species,
+                               function(y) extractSeqlevels(y, style)) 
+                
+        mres <- sapply(allseqlevels, function(y) intersect(y, ori_seqlevels))
+        
+        standard_chromosomes <- unique(unlist(mres))
+        standard_chromosomes <- 
+            ori_seqlevels[which(ori_seqlevels %in% standard_chromosomes)]
+        
+        if(length(standard_chromosomes) == 0 )
+            stop("Cannot determine standard chromosomes, Specify species arg")
+        
+    }else{
+        standard_chromosomes <- extractSeqlevels(species, style)
+        standard_chromosomes <- intersect(ori_seqlevels,standard_chromosomes)
+    }
+    
+    #x <- keepSeqlevels(x,standard_chromosomes)
+    seqlevels(x,force=TRUE) <- standard_chromosomes 
+    return(x)
+    
+}
+
---
+++ tests/runTests.R
@@ -0,0 +1,3 @@
+## BiocGenerics:::testPackage("GenomeInfoDb")
+require("GenomeInfoDb") || stop("unable to load GenomeInfoDb package")
+GenomeInfoDb:::.test()
